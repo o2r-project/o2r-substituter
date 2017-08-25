bf42b0a93b3d793627d55fbee7e3a2c4048a05bc
TeKraft <t.kraf03@gmail.com>
2017-08-25 13:13:52 +0200

    add request: list all substitutions

---
+++ controllers/substitutions.js
@@ -0,0 +1,48 @@
+/*
+ * (C) Copyright 2016 o2r project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+const config = require('../config/config');
+const debug = require('debug')('substituter-compendium');
+const exec = require('child_process').exec;
+
+var Compendium = require('../lib/model/compendium');
+
+exports.view = (req, res) => {
+  console.log("req.query");
+  console.log(req.query);
+  var answer = {};
+  var filter = { "metadata.substituted": true };
+  var limit = parseInt(req.query.limit || config.list_limit, 10);
+  var start = parseInt(req.query.start || 1, 10) - 1;
+
+  Compendium.find(filter).select('id').skip(start).limit(limit).exec((err, comps) => {
+    if (err) {
+      res.status(500).send(JSON.stringify({ error: 'query failed' }));
+    } else {
+      var count = comps.length;
+      if (count <= 0) {
+        res.status(404).send(JSON.stringify({ error: 'no compendium found' }));
+      } else {
+
+        answer.results = comps.map(comp => {
+          return comp.id;
+        });
+        res.status(200).send(JSON.stringify(answer));
+      }
+    }
+  });
+};
